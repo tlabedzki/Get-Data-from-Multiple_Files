be122fefa0114c699f511d73a0c27b2bebd8323c
Tomasz Łabędzki <tomaszlab@gmail.com>
2022-01-20 18:36:16 +0100

    Change in gitignore file, and delete smaller unused methods from main script.

--- scripts/main_script.py
+++ scripts/main_script.py
@@ -8,37 +8,19 @@
 
 import pandas as p
 import glob as g
-import matplotlib.pyplot as plt
 
 
 # Settings and paths
-path_to_files = '.\\..\\data\\mag*.txt'
-path_to_file_with_cities = '.\\..\\data\\mag_def.txt'
+path_to_files = './../data/mag*.txt'
 files = g.glob(path_to_files)
 
 # CSV read settings
 separator = '|'
 encoding_type = 'ISO-8859-1'
 
-# Filtering settings
-filterby_col_name = 'my_date'
-from_date = '2020-01-01'
-to_date = '2021-01-01'
-
-# Sorting settings
-groupby_col_name = 'mag'
-sortby_col_name = 'netto'
-
-# Join settings
-column_name_for_join = 'mag'
-
-# Chart settings
-date_column = 'my_date'
-values_columns = 'netto'
-
 # Exceptions in read files, fe. '.\\folder\\name_of_file.txt'
 exceptions = [
-    '.\\..\\data\\mag_def.txt'
+    './../data/mag_def.txt'
 ]
 
 
@@ -63,62 +45,8 @@ def concat_data_from_path(separator):
     return concated_data
 
 
-# Method for filter the data by date
-def filtering_by_data(concated_data, filterby_col_name, from_date, to_date):
-
-    result_data = concated_data.loc[(concated_data[filterby_col_name] >= from_date) & (concated_data[filterby_col_name] < to_date)]
-    print(result_data)
-    return result_data
-
-
-# Method for group and sort the data to show the table with the highest values on top
-def groupedby_and_sorting_by_data(concated_data, groupby_col_name, sortby_col_name):
-
-    result_data = concated_data.groupby(by=groupby_col_name).sum()
-    result_data = result_data.sort_values(by=sortby_col_name, ascending=False).reset_index()
-    print(result_data)
-    return result_data
-
-
-# Method for join two tables
-def join_two_tables(concated_data, path_to_file_with_cities, column_name_for_join, separator, encoding_type):
-
-    file_with_cities = load_csv_file(path_to_file_with_cities, separator, encoding_type)
-
-    result_data = concated_data.set_index(column_name_for_join).join(file_with_cities.set_index(column_name_for_join), how='inner')
-    print(result_data)
-    return result_data
-
-
-# Method for make simple chart with values and data
-def make_and_show_chart(concated_data, date_column):
-
-    concated_data[date_column] = p.to_datetime(concated_data[date_column])
-    ax = concated_data.plot(x=date_column, y=values_columns, figsize=(12,8))
-    plt.show()
-
-
-# Method for print info about data
-def print_info_about_data(concated_data):
-
-    print(concated_data.info())
-
-
-# Method for print info about installed libraries like pandas & numpy
-def check_version_of_installed_packages():
-
-    import pandas
-    import numpy
-    print('pd:{}'.format(pandas.__version__))
-    print('pd:{}'.format(numpy.__version__))
-
-
 def process_the_data():
-    concated_data = concat_data_from_path(separator)
-    filtering_by_data(concated_data, filterby_col_name, from_date, to_date)
-    groupedby_and_sorting_by_data(concated_data, groupby_col_name, sortby_col_name)
-    join_two_tables(concated_data, path_to_file_with_cities, column_name_for_join, separator, encoding_type)
-    make_and_show_chart(concated_data, date_column)
+    concat_data_from_path(separator)
 
 
 # Run scripts
